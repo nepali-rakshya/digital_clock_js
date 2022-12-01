335ff027b679f1e6adc75bc6d759bc24d3d2fab0
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-01 21:44:28 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// let sec = new Date();
+
+// console.log(sec.getSeconds());
+// let secStand = sec.setMinutes(1);
+// console.log(secStand);
+
+// // setInterval(function () {
+// //   if (sec.getSeconds() === sec.setSeconds(59)) {
+// //     console.log("it's same");
+// //   } else {
+// //     console.log("it's not same");
+// //   }
+// // }, 1000);
